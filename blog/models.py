ec5d898efb7abb9ce40e9760fe8da640ba07c0a2
Jerin <jerinthomas17@gamil.com>
2020-03-22 02:08:23 +0530

    home page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+# create blog model
+class Blog(models.Model):
+	title = models.CharField(max_length=255)
+	pub_date = models.DateTimeField()
+	body = models.TextField()
+	image = models.ImageField(upload_to = 'images/')
+
+
+# add blog app to settings	
+# BlogConfig in apps.py
+
+# create migration
+# python manage.py makemigrations
+
+# migrate
+# python manage.py migrate
+
+# add to admin
+# from .models import Blog
+
+# admin.site.register(Blog)
