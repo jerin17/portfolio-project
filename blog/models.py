20f13f26bdcc87a62bf6ec7cb527db562809bb6a
Jerin <jerinthomas17@gamil.com>
2020-03-23 14:59:13 +0530

    django portfolio basic - complete

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,16 @@ class Blog(models.Model):
 	body = models.TextField()
 	image = models.ImageField(upload_to = 'images/')
 
+	def summary(self):
+		return self.body[:100] + ' . . . '
+
+	def new_dt(self):
+		return self.pub_date.strftime('%b %e %Y')
+
+	def __str__(self):
+		return self.title
+
+
 
 # add blog app to settings	
 # BlogConfig in apps.py
